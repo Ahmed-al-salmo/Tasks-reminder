e011f1cf4006674e23a3a2d9824e04222ff3fb91
ahmed <salmoahmad079@gmail.com>
2026-09-08 01:12:29 +0300

    resize header and image

--- src/components/chatHeader.tsx
+++ src/components/chatHeader.tsx
@@ -3,7 +3,7 @@ import { FaUser } from "react-icons/fa";
 
 export default function ChatHeader({userName,theme,image}:{userName:string,theme:string,image:string}){
     return(
-        <div className={`w-full h-[60px]  flex items-center pl-15 border-b-1 border-gray-300 ${theme==='dark'? 'bg-gray-700 text-white': 'bg-gray-200'}`}>
+        <div className={`w-full h-[80px]  flex items-center pl-15 border-b-1 border-gray-300 ${theme==='dark'? 'bg-gray-700 text-white': 'bg-gray-200'}`}>
             {
             image ? 
                 <img src={image} alt="" className={'w-[40px] h-[40px] rounded-full mx-3 '}  />:
